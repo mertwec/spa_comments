5f0a1e903720043d161abda8463478ec3678ff51
Walentin <seryimir@gmail.com>
2023-03-14 10:43:25 +0200

    v0.1 before rework

--- comments/models.py
+++ comments/models.py
@@ -2,14 +2,6 @@ from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
 
 
-# class User(models.Model):
-#     username = models.CharField(max_length=50, null=False)
-#     email = models.EmailField(null=False)
-    
-#     def __str__(self) -> str:
-#         return f'user: {self.username}'
-
-
 class MockPost(models.Model):
     title = models.CharField(max_length=50, default="Comments")
 
@@ -39,3 +31,49 @@ class Comment(MPTTModel):
     
     def __str__(self):
         return f"comment {self.username}:  {self.text}"
+
+
+# class User(models.Model):
+#     username = models.CharField(max_length=50, null=False)
+#     email = models.EmailField(null=False)
+    
+#     def __str__(self) -> str:
+#         return f'user: {self.username}'
+
+
+# class Post(models.Model):
+#     title = models.CharField(max_length=50, default="Comments")
+#     text = models.TextField(null=False, blank=False)
+#     url = models.URLField(null=True, blank=True)
+
+#     owner = models.ForeignKey(
+#         User, related_name='posts', on_delete=models.CASCADE)
+
+#     created_at = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self) -> str:
+#         return f'Post: {self.title} owner {self.owner}'
+
+
+# class Comment(MPTTModel):
+
+#     post = models.ForeignKey(
+#         Post, related_name='comments', on_delete=models.CASCADE)
+    
+#     owner = models.ForeignKey(
+#         User, related_name='comments', on_delete=models.CASCADE)
+    
+#     parent = TreeForeignKey('self', null=True, blank=True, related_name='children', db_index=True,
+#                                    on_delete=models.CASCADE)
+
+#     text = models.TextField(null=False, blank=False)
+#     url = models.URLField(null=True, blank=True)
+
+#     created_at = models.DateTimeField(auto_now_add=True)
+
+#     class MpttMeta:
+#         order_insertion_by = ['created_on']
+#         db_table = 'comments'
+    
+#     def __str__(self):
+#         return f"comment {self.username}:  {self.text}"
--- comments/views.py
+++ comments/views.py
@@ -9,57 +9,38 @@ import pdb
 def block_comments(request):
 
     post = MockPost.objects.first()
-
     if not post:
         post = MockPost.objects.create(title='Discussion')
+
     form = NewCommentForm()
 
+    if request.method == "POST":
+        form = NewCommentForm(request.POST)
+        pdb.set_trace()
+        if form.is_valid():
+            new_comm = form.save(commit=False)
+            new_comm.post_id = post.pkc
+            new_comm.parrent_id = form["parent"].value()
+            new_comm.save()
+            return redirect('/')
+
     all_comments = Comment.objects.all()
-    items_on_page = 3
+    zero_comments = Comment.objects.all().filter(level=0)
+    items_on_page = 25
     page = split_on_pages(
         request=request,
         queryset=all_comments,
         items_on_page=items_on_page)
 
     # pdb.set_trace()
-
-    form = NewCommentForm(request.POST)
-    if form.is_valid():
-        new_comm = form.save(commit=False)
-        new_comm.post_id = post.pk
-        new_comm.parrent_id = form["parent"].value()
-        new_comm.save()
-        return redirect('/')
-
-    # try:
-    output = render(
+    return render(
         request,
         template_name="comments/content.html",
         context={
             "title": post.title,
             "comments": page.object_list,
-            # "comments": all_comments,
+            # "all_comments": all_comments,
             "page": page,
             "comment_form": form
         }
     )
-    # except ValueError:
-    #     items_on_page += 1
-    #     page = split_on_pages(
-    #         request=request,
-    #         queryset=all_comments,
-    #         items_on_page=items_on_page)
-
-    #     output = render(
-    #         request,
-    #         template_name="comments/content.html",
-    #         context={
-    #             "title": post.title,
-    #             "comments": page.object_list,
-    #             # "comments": all_comments,
-    #             "page": page,
-    #             "comment_form": form
-    #         }
-    #     )
-
-    return output
